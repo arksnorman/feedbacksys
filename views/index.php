2443892746f928f7c574b8878a8e1a6c4d2610c2
arksnorman <arksnorman@gmail.com>
2017-04-27 14:47:23 +0300

    Removed welcome controller and added home controller

--- views/index.php
+++ views/index.php
@@ -82,6 +82,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
 
+	<p>BASE_PATH: <?php echo BASEPATH; ?></p>
+	<p>SYSDIR: <?php echo SYSDIR; ?></p>
+	<p>SELF: <?php echo SELF; ?></p>
+	<p>FCPATH: <?php echo FCPATH; ?></p>
+
+
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
